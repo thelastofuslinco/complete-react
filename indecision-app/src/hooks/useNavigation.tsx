2dd4a32de419a2fc82f1cbf87273506074f40d6a
lincoln Duarte <lincolnskyrim_99@hotmail.com>
2023-10-16 08:06:50 -0300

    refactor: remove hook

--- indecision-app/src/hooks/useNavigation.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react'
-import { NavigationContext } from '../context/navigation'
-
-const useNavigation = () => {
-  const context = useContext(NavigationContext)
-  return context
-}
-
-export default useNavigation
