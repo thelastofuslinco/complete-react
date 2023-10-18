7d024df484e5ffd47c7d946c984e50e40e3f70d9
lincoln Duarte <lincolnskyrim_99@hotmail.com>
2023-10-17 22:27:36 -0300

    chore: add normalize

--- indecision-app/src/index.js
+++ indecision-app/src/index.js
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import React from 'react'
 import App from './App'
+import 'normalize.css'
 import './styles/styles.scss'
 import NavigationProvider from './context/navigation'
 
